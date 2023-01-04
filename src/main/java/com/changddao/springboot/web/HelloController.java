4ca7a5aff21c0b6d869c9effc7e985c80db74bcd
Changhogod <younch8342@naver.com>
2023-01-05 00:29:07 +0900

    Initial commit

---
+++ src/main/java/com/changddao/springboot/web/HelloController.java
@@ -0,0 +1,2 @@
+package com.changddao.springboot.web;public class HelloController {
+}
---
+++ src/test/java/com/changddao/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changddao.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
